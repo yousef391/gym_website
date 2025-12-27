a6386807aff2486b92f84ba5d4a4e7e6568db4ca
yousefouddane391 <y.ouddane@yayglobaldz.com>
2025-12-27 21:04:43 +0100

    fix: remove terser minifier

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
-    minify: "terser",
+    minify: true,
     rollupOptions: {
       output: {
         manualChunks: {
